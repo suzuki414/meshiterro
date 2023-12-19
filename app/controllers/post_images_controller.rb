20cb86dfd27d8004527584f75427a0c820fd7173
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-19 13:14:09 +0000

    finish 4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @postimage = PostImage.new
   end
-  
+
   def create
     @postimage = PostImage.new(postimage_params)
     @postimage.user_id = current_user.id
@@ -17,9 +17,15 @@ class PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
   end
-  
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end
+
   private
-  
+
   def postimage_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
