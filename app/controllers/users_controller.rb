8498c0d71cda25bf4c2fa39f7582a7ad671283f4
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-21 03:27:07 +0000

    deviseの初期設定を学ぼう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_image.page(params[:page])
   end
+  
 
   def edit
     @user = User.find(params[:id])
@@ -14,10 +16,16 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
   
-  
   private
   
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
